41e9f59617abd1f40d2ee369a12123493244ec5e
板楠　亮 <ir060906@gmail.com>
2021-11-12 11:55:23 +0900

    first commit

--- app.js
+++ app.js
@@ -1,49 +1,104 @@
-let hello = 'Hello World'
-alert(hello);
+// let user_hand = prompt('グー、チョキ、パー');
+// while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+//   alert('グー・チョキ・パーのどれか');
+//   user_hand = prompt('今度はグー、チョキ、パー頼むよ！');
+// }
 
-let str1 = 'Hello';
-let str2 = 'World';
-alert(str1 + str2);
+// let js_hand = getJShand();
 
-let orange = 100;
-let apple = 120;
+// let judge = winLose(user_hand,js_hand);
 
-if(orange < apple){
-  alert('みかんの値段がりんごより安い');
-} else if(orange == apple){
-  alert('みかんとりんごが同じ値段');
-} else{
-  alert('みかんの値段がりんごより高い');
-}
+// if (user_hand != null){
+// alert('選んだ手は' + user_hand + 'です' + 'javascriptは' + js_hand + 'です。結果は' + judge +'じゃ');
+// }else{
+//   alert("またね")
+// }
 
-let ore = 1000;
-let app = 1200;
+// function getJShand(){
+//   let js_hand_num = Math.floor(Math.random()*3);
+//   let hand_name
 
-if(ore < app){
-  alert('みかんが安い');
-} else if(ore == app){
-  alert('同じ');
-} else{
-  alert('りんごが高い');
-}
-
-let max = 10500;
-let num = 1;
-let count = 0;
-
-while(num < max){
-  num = num * 2;
-  count = count + 1;
-}
-
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+//   if(js_hand_num == 0){
+//     hand_name = "グー"
+//   }else if(js_hand_num == 1){
+//     hand_name = "チョキ"
+//   }else if(js_hand_num == 2){
+//     hand_name = "パー"
+//   }
+//   return hand_name;
+// }
 
 
-let i;
-let num = 0;
+// function winLose(user,js){
+//   let winLoseStr;
 
-for(i = 1; i < 11; i++){
-  num = num + i;
-}
+//   if(user == "グー"){
+//    if(js == "グー"){
+//      WinLoseStr == "あいこ";
+//    } else if(js == "チョキ"){
+//     winLoseStr = "勝ち";
+//   } else if(js == "パー"){
+//     winLoseStr = "負け";
+//   }
+// } else if(user == "チョキ"){
+//   if(js == "グー"){
+//     winLoseStr = "負け";
+//   } else if(js == "チョキ"){
+//     winLoseStr = "あいこ";
+//   } else if(js == "パー"){
+//     winLoseStr = "勝ち";
+//   }
+// } else if(user == "パー"){
+//   if(js == "グー"){
+//     winLoseStr = "勝ち";
+//   } else if(js == "チョキ"){
+//     winLoseStr = "負け";
+//   } else if(js == "パー"){
+//     winLoseStr = "あいこ";
+//   }
+// }
 
-alert('1から10まで足し算した結果は' + num + 'です');
+// return winLoseStr;
+// }
+ 
+// １章
+// alert('Hello World')
+
+// let a = "Hello World"
+// alert(a);
+
+// let int1 = 1;
+// alert(int1);
+
+
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert('みかんよりりんごが高い');
+// }else if(orange == apple){
+//   alert('同じ値段');
+// }else{
+//   alert('りんごが高い');
+// }
+
+// let max = 1000;
+// let num = 100;
+// let count = 0;
+
+// while(num < max){
+//   num = num*2;
+//   count = count + 1;
+// }
+// alert('２をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// let i =　0;
+// let num = 0;
+
+// for(i; i < 11; i++){
+//   num = num + i;
+// }
+
+// prompt('1から10まで足し算した結果は' + num + 'です');
+
+// ３章
